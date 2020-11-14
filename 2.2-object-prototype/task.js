3d18bb0f2886fc1293750956094e2ce6f55f0e2c
Yana Chumburidze <ya.chum@gmail.com>
2020-11-14 22:21:27 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,40 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+// Задача 1
+String.prototype.isPalindrome = function() {
+    let str = this;
+    str = str.replace(/\s+/g, '');
+    let reversedStr = str.split('').reverse().join('');
+    reversedStr = reversedStr.replace(/\s+/g, '');
+
+    return str.toUpperCase() === reversedStr.toUpperCase();
 }
 
+
+// Задача 2
+function getAverageMark(marks) {
+    let marksLength = marks.length;
+    let sum = 0;
+
+    if (!marks || marksLength === 0) {
+        return 0;
+    }
+
+    for (let index = 0; index < marksLength; index++) {
+        sum += marks[index];
+    }
+
+    const roundedAverage = Math.round(sum / marksLength);
+
+    return roundedAverage;
+}
+
+// Задача 3
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = Date.now();
+    birthday = new Date(birthday).getTime();
+    const diff = now - birthday;
+    const age = diff / (24 * 3600 * 365.25 * 1000);
+
+    return age > 18;
 }
