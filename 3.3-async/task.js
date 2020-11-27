c5fea9ded09aa2daaf06b9ffed597a2436415314
Yana Chumburidze <ya.chum@gmail.com>
2020-11-27 09:37:45 +0300

    second commit async

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -85,3 +85,26 @@ class AlarmClock {
         this.alarmCollection = [];
     }
 }
+
+function testCase() {
+    let phoneAlarm = new AlarmClock();
+
+    phoneAlarm.addClock('09:00', () => console.log('Пора вставать'), 1);
+    phoneAlarm.addClock('09:01', () => { console.log('Давай, вставай уже!');
+        phoneAlarm.removeClock(2) }, 2);
+    phoneAlarm.addClock('09:01', () => console.log('Иди умываться!'));
+
+    phoneAlarm.addClock('09:02', () => {
+        console.log('Вставай, а то проспишь!');
+        phoneAlarm.clearAlarms();
+        phoneAlarm.printAlarms();
+    }, 3);
+
+    phoneAlarm.addClock('09:05', () => console.log('Вставай, а то проспишь!'), 1);
+
+    phoneAlarm.printAlarms();
+
+    phoneAlarm.start();
+}
+
+testCase();
